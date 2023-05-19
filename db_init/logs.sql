1161807e9fba4ffe26312b500acd5394a92bb0af
arthur.hav <arthurhavlicek@gmail.com>
2023-05-19 14:40:41 +0200

    Remove filters

--- db_init/logs.sql
+++ db_init/logs.sql
@@ -12,8 +12,3 @@ SELECT set_chunk_time_interval('logs', INTERVAL '10 minutes');
 ALTER TABLE logs SET (timescaledb.compress);
 SELECT alter_job((SELECT add_compression_policy('logs', INTERVAL '20 minutes')), schedule_interval => INTERVAL '20 minutes');
 SELECT alter_job((SELECT add_retention_policy('logs', INTERVAL '7 days')), schedule_interval => INTERVAL '7 days');
-
-CREATE TABLE filters (
-    id BIGSERIAL PRIMARY KEY,
-    list_filters JSONB
-);
